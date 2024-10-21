d8754ce4bb132b15537d4dbb07ce8689c386afaf
Ihor Makhnyk <ingvarus17@gmail.com>
2024-10-21 20:20:04 +0300

    Remove stuff not related to LLMs usage

--- Package.swift
+++ Package.swift
@@ -10,13 +10,10 @@ let package = Package(
         .library(
             name: "LLM",
             targets: ["MLXLLM"]),
-        .library(
-            name: "MNIST",
-            targets: ["MLXMNIST"]),
     ],
     dependencies: [
         .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.16.1"),
-        .package(url: "https://github.com/huggingface/swift-transformers", from: "0.1.12"),
+        .package(url: "https://github.com/ihor-makhnyk/swift-transformers", branch: "main"),
         .package(url: "https://github.com/1024jp/GzipSwift", "6.0.1" ... "6.0.1"),
         .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.0"),
     ],
@@ -35,35 +32,6 @@ let package = Package(
             exclude: [
                 "README.md",
                 "LLM.h",
-            ]
-        ),
-        .target(
-            name: "MLXMNIST",
-            dependencies: [
-                .product(name: "MLX", package: "mlx-swift"),
-                .product(name: "MLXFast", package: "mlx-swift"),
-                .product(name: "MLXNN", package: "mlx-swift"),
-                .product(name: "MLXOptimizers", package: "mlx-swift"),
-                .product(name: "MLXRandom", package: "mlx-swift"),
-                .product(name: "Transformers", package: "swift-transformers"),
-                .product(name: "Gzip", package: "GzipSwift"),
-            ],
-            path: "Libraries/MNIST",
-            exclude: [
-                "README.md",
-                "MNIST.h",
-            ]
-        ),
-        .target(
-            name: "StableDiffusion",
-            dependencies: [
-                .product(name: "MLX", package: "mlx-swift"),
-                .product(name: "MLXNN", package: "mlx-swift"),
-                .product(name: "MLXRandom", package: "mlx-swift"),
-            ],
-            path: "Libraries/StableDiffusion",
-            exclude: [
-                "README.md"
             ]
         ),
     ]
